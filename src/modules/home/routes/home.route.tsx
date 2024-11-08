bd2cdd84b3f22ebea6b7d868506c4dc083d7baad
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-08 21:33:27 +0500

    feat: HeaderComponent registered and add divider

--- src/modules/home/routes/home.route.tsx
+++ src/modules/home/routes/home.route.tsx
@@ -1,24 +1,47 @@
 import { ApartmentCard } from '#/modules/home/components/card';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import { HeaderComponent } from '../components/header';
+import { Container, Divider } from '@mui/material';
 
 export function HomeRoute() {
   return (
-    <Box>
-      <Typography variant="h2">Home sweet home</Typography>
-      <Typography>Welcome to the home route.</Typography>
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)',
-          gap: 3,
-        }}
-      >
-        <ApartmentCard />
-        <ApartmentCard />
-        <ApartmentCard />
-        <ApartmentCard />
-      </Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+      <HeaderComponent />
+      <Container maxWidth="xl">
+        <Divider
+          sx={{
+            mt: { xs: 8, sm: 10, md: 12 },
+            mb: { xs: 4, sm: 5, md: 6 },
+            borderColor: '#DDDDDD',
+            borderBottomWidth: 1,
+            width: '100%',
+            opacity: 1,
+            '&::before, &::after': {
+              borderColor: '#DDDDDD',
+            },
+          }}
+        />
+
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: {
+              xs: '1fr',
+              sm: 'repeat(2, 1fr)',
+              md: 'repeat(3, 1fr)',
+              lg: 'repeat(4, 1fr)',
+            },
+            gap: { xs: 2, sm: 3, md: 4 },
+            px: { xs: 2, sm: 3, md: 4 },
+            py: { xs: 2, sm: 3, md: 4 },
+          }}
+        >
+          <ApartmentCard />
+          <ApartmentCard />
+          <ApartmentCard />
+          <ApartmentCard />
+        </Box>
+      </Container>
     </Box>
   );
 }
