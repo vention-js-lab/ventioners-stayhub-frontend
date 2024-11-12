024009fb4c4043eb82e18de9988327c6a5abc47c
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-11-12 20:31:04 +0500

    fix: resolve comments

--- src/modules/home/components/property/propertyCard.tsx
+++ src/modules/home/components/property/propertyCard.tsx
@@ -4,11 +4,13 @@ import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { styles } from './property.styles';
 
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './swiper.css';
 
 import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 
@@ -34,20 +36,7 @@ export function ApartmentCard(apartment: Accommodation) {
   return (
     <Link to={`/apartment/${apartment.id}`} style={{ textDecoration: 'none' }}>
       <Card sx={{ boxShadow: 'none', position: 'relative' }}>
-        <FavoriteBorderIcon
-          onClick={handleFavoriteClick}
-          sx={{
-            position: 'absolute',
-            top: 8,
-            right: 8,
-            zIndex: 2,
-            color: 'white',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            borderRadius: '50%',
-            padding: '4px',
-            fontSize: '32px',
-          }}
-        />
+        <FavoriteBorderIcon onClick={handleFavoriteClick} sx={styles.favoriteIconStyle} />
         <Swiper
           style={{ height: '240px', aspectRatio: '1/1', width: '100%', borderRadius: '12px' }}
           spaceBetween={30}
@@ -60,8 +49,8 @@ export function ApartmentCard(apartment: Accommodation) {
           modules={[EffectFade, Navigation, Pagination]}
           className="mySwiper"
         >
-          {apartment.images.map((image) => (
-            <SwiperSlide>
+          {apartment.images.map((image, index) => (
+            <SwiperSlide key={index}>
               <img src={image} alt="Image of Apartment" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </SwiperSlide>
           ))}
@@ -75,41 +64,6 @@ export function ApartmentCard(apartment: Accommodation) {
           </Typography>
         </CardContent>
       </Card>
-      <style>{`
-
-       .mySwiper .swiper-button-next,
-        .mySwiper .swiper-button-prev {
-          opacity: 0;
-          transition: opacity 0.3s ease;
-        }
-        /* Show navigation buttons on hover */
-        .mySwiper:hover .swiper-button-next,
-        .mySwiper:hover .swiper-button-prev {
-          opacity: 1;
-        }
-
-        .swiper-button-next:after,
-        .swiper-button-prev:after,
-        .swiper-button-next:before,
-        .swiper-button-prev:before {
-          height: 15px;
-          font-size: 15px; 
-          color: #000;
-          background-color: #fff; 
-          border-radius: 50%; /* Make buttons round */
-        }
-        .swiper-button-next,
-        .swiper-button-prev{
-          color: #000;
-          background-color: #fff; 
-          border-radius: 50%;
-          padding: 0 12px;
-          height: 28px;
-        }
-        .swiper-pagination-bullet{
-          background-color: #fff;
-        }
-      `}</style>
     </Link>
   );
 }
