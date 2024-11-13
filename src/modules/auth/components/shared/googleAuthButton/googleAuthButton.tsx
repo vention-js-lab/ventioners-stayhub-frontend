1677dc5b9e90902880585a19dc196391bd8ccec1
tiescl <anvar.madvaliev@icloud.com>
2024-11-13 23:52:32 +0500

    feat: integrate GoogleAuth with the backend, minor styling changes

--- src/modules/auth/components/shared/googleAuthButton/googleAuthButton.tsx
+++ src/modules/auth/components/shared/googleAuthButton/googleAuthButton.tsx
@@ -3,19 +3,16 @@ import googleIcon from '#/modules/auth/assets/icons/google.svg';
 import styles from './googleAuthButton.module.css';
 
 export function GoogleAuthButton() {
-  function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
-    console.log(e.target);
-    console.log('continue with google');
-  }
-
   return (
-    <button className={styles.wrapper} onClick={handleClick}>
-      <Box className={styles.container}>
-        <Box className={styles.logo}>
-          <img src={googleIcon} alt="Google Icon" />
-        </Box>
-        <Box className={styles.text}>Continue with Google</Box>
+    <form action="http://localhost:3000/api/auth/google/login">
+      <Box className={styles.wrapper}>
+        <button type="submit" className={styles.container}>
+          <Box className={styles.logo}>
+            <img src={googleIcon} alt="Google Icon" />
+          </Box>
+          <Box className={styles.text}>Continue with Google</Box>
+        </button>
       </Box>
-    </button>
+    </form>
   );
 }
