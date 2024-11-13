230314bfaff20177a3e9fa052bc2a780737a169c
Ismoil <120241601+Ismoil-JS@users.noreply.github.com>
2024-11-13 14:35:13 +0500

    [IT]:[17-Property-Card] feat: property card component has been created (#6)
    * feat: property card component has been created
    
    * fix: resolve comments

---
+++ src/modules/home/components/property/property.styles.ts
@@ -0,0 +1,15 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+export const styles: Record<string, SxProps<Theme>> = {
+  favoriteIconStyle: {
+    position: 'absolute',
+    top: 8,
+    right: 8,
+    zIndex: 2,
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: '50%',
+    padding: '4px',
+    fontSize: '32px',
+  },
+};
--- src/modules/home/components/property/propertyCard.tsx
+++ src/modules/home/components/property/propertyCard.tsx
@@ -1,16 +1,69 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { styles } from './property.styles';
 
-export function ApartmentCard() {
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './swiper.css';
+
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+type Accommodation = {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  location: string;
+  price_per_night: number;
+  type: string;
+  amenities: string[];
+  status: string;
+};
+
+export function ApartmentCard(apartment: Accommodation) {
+  const handleFavoriteClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log('Favorite icon clicked');
+  };
+
   return (
-    <Card>
-      <CardMedia component="img" height="140" image="https://placehold.co/600x400" alt="Apartment" />
-      <CardContent>
-        <Typography variant="h5">Apartment</Typography>
-        <Typography>Beautiful apartment in the city center</Typography>
-      </CardContent>
-    </Card>
+    <Link to={`/apartment/${apartment.id}`} style={{ textDecoration: 'none' }}>
+      <Card sx={{ boxShadow: 'none', position: 'relative' }}>
+        <FavoriteBorderIcon onClick={handleFavoriteClick} sx={styles.favoriteIconStyle} />
+        <Swiper
+          style={{ height: '240px', aspectRatio: '1/1', width: '100%', borderRadius: '12px' }}
+          spaceBetween={30}
+          effect={'fade'}
+          loop={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          {apartment.images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt="Image of Apartment" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <CardContent sx={{ padding: '16px 0px' }}>
+          <Typography variant="h6">{apartment.name}</Typography>
+          <Typography sx={{ opacity: '.7' }}>{apartment.location}</Typography>
+          <Typography>
+            <strong>${apartment.price_per_night}</strong> night
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
