a7dd1426257c30cedc80486242836b3ad5c3e17f
tiescl <anvar.madvaliev@icloud.com>
2024-11-14 16:51:21 +0500

    feat: add an interceptor to handle refresh token logic

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -11,3 +11,22 @@ export const axiosInstance = axios.create({
     Authorization: `Bearer ${AccessTokenStore.getToken()}`,
   },
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response && error.response.status === 401) {
+      try {
+        await axiosInstance.get('/auth/refresh-token');
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
