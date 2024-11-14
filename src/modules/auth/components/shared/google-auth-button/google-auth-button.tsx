fd51412a62d541335685915b864ad808fd4e4cb8
tiescl <anvar.madvaliev@icloud.com>
2024-11-15 01:57:00 +0500

    fix: handle the accessToken received after successful Google sign in

--- src/modules/auth/components/shared/google-auth-button/google-auth-button.tsx
+++ src/modules/auth/components/shared/google-auth-button/google-auth-button.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { ENDPOINTS } from '#/modules/auth/constants';
 import googleIcon from '#/modules/auth/assets/icons/google.svg';
 import styles from './google-auth-button.module.css';
+import { AccessTokenStore } from '#/utils';
 
 export function GoogleAuthButton() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get('accessToken');
+
+    if (accessToken) {
+      AccessTokenStore.setToken(accessToken);
+      navigate('/');
+    }
+  }, []);
+
   return (
     <form action={`${import.meta.env.VITE_API_URL}${ENDPOINTS.googleAuth}`}>
       <Box className={styles.wrapper}>
