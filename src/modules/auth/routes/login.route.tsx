5a4db063ea3ff3e42241177d9a084e5f86c38913
tiescl <anvar.madvaliev@icloud.com>
2024-11-17 23:55:07 +0500

    refactor: convert auth page styles to mui styles

--- src/modules/auth/routes/login.route.tsx
+++ src/modules/auth/routes/login.route.tsx
@@ -1,16 +1,16 @@
 import { Box } from '@mui/material';
 import { LoginForm } from '../components';
-import styles from '../styles/auth-page.module.css';
+import styles from '../styles/auth-page.sx';
 
 export function LoginRoute() {
   return (
-    <Box className={styles.container}>
-      <Box className={styles.wrapper}>
-        <Box className={styles.header}>
-          <Box className={styles.title}>Log in</Box>
+    <Box sx={styles.container}>
+      <Box sx={styles.wrapper}>
+        <Box sx={styles.header}>
+          <Box sx={styles.title}>Log in</Box>
         </Box>
-        <Box className={styles.main}>
-          <Box className={styles.heading}>Welcome to StayHub</Box>
+        <Box sx={styles.main}>
+          <Box sx={styles.heading}>Welcome to StayHub</Box>
 
           <LoginForm />
         </Box>
