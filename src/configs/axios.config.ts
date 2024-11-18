c0e8131f8d7a5a60ac9daf5afdf98c863bc873ab
Akilbek Urinbaev <akilbekurinbaev0616@gmail.com>
2024-11-18 18:41:50 +0500

    fix: remover access token file

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -9,24 +9,3 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error('API error:', error.response || error.message);
-    return Promise.reject(error.response || error.message);
-  }
-);
