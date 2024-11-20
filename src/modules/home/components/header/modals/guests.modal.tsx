dc8e0085744f111f9bc0d48376a7ad4f1f69a424
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-20 06:30:30 +0500

    fix: guest types constant seperated from componnent

--- src/modules/home/components/header/modals/guests.modal.tsx
+++ src/modules/home/components/header/modals/guests.modal.tsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { CounterButton, CounterText } from '#/modules/home/styles';
+import { guestTypes } from '../constants';
 
 export interface GuestCounts {
   adults: number;
@@ -23,37 +24,6 @@ interface GuestsModalProps {
 }
 
 export function GuestsModal({ open, onClose, guestCounts, onGuestCountsChange }: GuestsModalProps) {
-  const guestTypes = [
-    {
-      type: 'adults',
-      title: 'Adults',
-      description: 'Ages 13 or above',
-      max: 16,
-      min: 0,
-    },
-    {
-      type: 'children',
-      title: 'Children',
-      description: 'Ages 2-12',
-      max: 8,
-      min: 0,
-    },
-    {
-      type: 'infants',
-      title: 'Infants',
-      description: 'Under 2',
-      max: 5,
-      min: 0,
-    },
-    {
-      type: 'pets',
-      title: 'Pets',
-      description: 'Service animals welcome',
-      max: 5,
-      min: 0,
-    },
-  ] as const;
-
   const handleCountChange = (type: keyof GuestCounts, increment: boolean) => {
     const guestType = guestTypes.find((g) => g.type === type);
     if (!guestType) return;
