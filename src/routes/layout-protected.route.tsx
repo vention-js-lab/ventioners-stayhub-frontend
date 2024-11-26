493978a76b87e5e1f5e0a601d30a70599163efeb
tiescl <anvar.madvaliev@icloud.com>
2024-11-26 23:07:48 +0500

    feat: add styling to spinner element

--- src/routes/layout-protected.route.tsx
+++ src/routes/layout-protected.route.tsx
@@ -3,8 +3,24 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '#/redux/hooks';
 import { createUser, removeUser } from '#/redux/auth/authSlice';
 import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
-const loadingStyles = { display: 'flex', backgroundColor: '#000000' };
+const styles = {
+  container: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  spinner: {
+    color: '#000000',
+  },
+};
 
 type Props = {
   protectedRoute: boolean;
@@ -16,7 +32,11 @@ export function AuthLayout({ protectedRoute }: Props) {
   const dispatch = useAppDispatch();
 
   if (isLoading) {
-    return <CircularProgress sx={loadingStyles} />;
+    return (
+      <Box sx={styles.container}>
+        <CircularProgress sx={styles.spinner} />
+      </Box>
+    );
   }
 
   if (isError || !user) {
