33b9177d5396a57c84ece514e3110bc8aa3c96d0
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-26 19:06:36 +0500

    feat: /host/homes route registered

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,6 +1,7 @@
 import { lazyRoute } from '#/utils/router.util';
 import { type RouteObject } from 'react-router-dom';
 import { WishlistRoute } from './routes/wishlist.route';
+import { CreateAccommodatioRoute } from './routes/create-acccommodation.route';
 
 const { HomeRoute } = lazyRoute(() => import('./routes/home.route'), 'HomeRoute');
 
@@ -12,5 +13,9 @@ export const HomeRoutes: RouteObject[] = [
   {
     path: '/wishlist',
     element: <WishlistRoute />,
+  },
+  {
+    path: '/host/homes',
+    element: <CreateAccommodatioRoute />,
   },
 ];
