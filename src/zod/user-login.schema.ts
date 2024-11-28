38a15aa2c6360b2bed4d605b6932c55cab2f9788
tiescl <anvar.madvaliev@icloud.com>
2024-11-28 14:50:26 +0500

    fix: remove login password validation

--- src/zod/user-login.schema.ts
+++ src/zod/user-login.schema.ts
@@ -3,5 +3,5 @@ import { type AuthFormData } from '#/modules/auth/types';
 
 export const UserLoginSchema: ZodType<Pick<AuthFormData, 'email' | 'password'>> = z.object({
   email: z.string().email({ message: 'Please enter a valid email (e.g. john@stayhub.com)' }),
-  password: z.string().min(8, { message: 'Entered password is too short' }).max(64, { message: 'Entered password is too long' }),
+  password: z.string(),
 });
