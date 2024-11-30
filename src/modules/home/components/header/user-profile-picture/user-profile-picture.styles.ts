d3f912afcb0e75fedfb78ae3f5612495ff6043c4
tiescl <anvar.madvaliev@icloud.com>
2024-11-30 05:08:40 +0500

    feat: final touches, backend integration for profile picture uploads

--- src/modules/home/components/header/user-profile-picture/user-profile-picture.styles.ts
+++ src/modules/home/components/header/user-profile-picture/user-profile-picture.styles.ts
@@ -2,6 +2,14 @@ import { type MuiStylesObject } from '#/types/mui-styles-object.type';
 
 export const userProfilePictureStyles = {
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyItems: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+
+  image: {
     width: '32px',
     height: '32px',
     large: {
@@ -24,5 +32,28 @@ export const userProfilePictureStyles = {
     fontSize: '15px',
     fontWeight: '400',
     textTransform: 'uppercase',
+  },
+  button: {
+    all: 'unset',
+    position: 'absolute',
+    bottom: '-10px',
+    display: 'flex',
+    alignItems: 'center',
+    paddingX: '16px',
+    paddingY: '6px',
+    cursor: 'pointer',
+    width: '82px',
+
+    color: 'rgb(34, 34, 34)',
+    backgroundColor: '#FFFFFF',
+    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 6px 16px',
+    fontWeight: 600,
+    fontSize: '14px',
+    borderRadius: '20px',
+
+    '& > img': {
+      width: '16px',
+      height: '16px',
+    },
   },
 } satisfies MuiStylesObject;
--- src/modules/home/components/header/user-profile-picture/user-profile-picture.tsx
+++ src/modules/home/components/header/user-profile-picture/user-profile-picture.tsx
@@ -1,6 +1,16 @@
+import { useRef } from 'react';
 import Box from '@mui/material/Box';
-import { userProfilePictureStyles as styles } from './user-profile-picture.styles';
+import Button from '@mui/material/Button';
+import cameraIcon from '#/assets/camera.svg';
 import { type User } from '#/types';
+import { userProfilePictureStyles as styles } from './user-profile-picture.styles';
+import { api } from '#/configs';
+import { ENDPOINTS } from '#/modules/users/constants';
+import { omit } from '#/utils';
+import { useQueryClient } from '@tanstack/react-query';
+import { useAppDispatch } from '#/redux/hooks';
+import { createUser } from '#/redux/auth/auth-slice';
+import { toast } from 'react-toastify';
 
 type Props = {
   user: User;
@@ -8,9 +18,57 @@ type Props = {
 };
 
 export function UserProfilePicture({ user, size }: Props) {
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
+  const queryClient = useQueryClient();
+  const dispatch = useAppDispatch();
+
+  function handleSubmit(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    api
+      .put(`${ENDPOINTS.users}/${user.id}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then((res) => {
+        const updatedUser = omit(res.data, ['createdAt', 'updatedAt', 'passwordHash']) as User;
+
+        queryClient.invalidateQueries({ queryKey: ['auth-user'] });
+        dispatch(createUser(updatedUser));
+        toast('Success');
+      })
+      .catch(() => toast('There was an error while saving your picture'));
+  }
+
   return (
-    <Box sx={{ ...styles.container, ...(size === 'large' && styles.container.large) }}>
-      <Box sx={{ ...styles.text, ...(size === 'large' && { fontSize: '32px' }) }}>{user.firstName[0]}</Box>
+    <Box sx={styles.container}>
+      <Box
+        sx={{
+          ...styles.image,
+          ...(size === 'large' && styles.image.large),
+          ...(user.profilePictureUrl && {
+            backgroundImage: `url(${user.profilePictureUrl})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }),
+        }}
+      >
+        {!user.profilePictureUrl && (
+          <Box sx={{ ...styles.text, ...(size === 'large' && { fontSize: '32px' }) }}>{user.firstName[0]}</Box>
+        )}
+
+        {size === 'large' && (
+          <Button disableRipple={true} sx={styles.button} onClick={() => imageInputRef.current?.click()}>
+            <img src={cameraIcon} />
+            &nbsp;Edit
+            <input type="file" ref={imageInputRef} style={{ display: 'none' }} onChange={handleSubmit} accept="image/*" />
+          </Button>
+        )}
+      </Box>
     </Box>
   );
 }
