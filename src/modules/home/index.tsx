7cc98ea8b72df5c6e5e436d15055c9d726cb1089
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-01 11:22:21 +0500

    feat: create acc route registered

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -2,6 +2,7 @@ import { lazyRoute } from '#/utils/router.util';
 import { type RouteObject } from 'react-router-dom';
 import { SinglePropertyRoute } from './routes/single-property.route';
 import { AuthLayout } from '#/routes/layout-protected.route';
+import { CreateAccommodationRoute } from '#/modules/home/routes/create-acccommodation.route.tsx';
 
 const { HomeRoute } = lazyRoute(() => import('./routes/home.route'), 'HomeRoute');
 const { WishlistRoute } = lazyRoute(() => import('./routes/wishlist.route'), 'WishlistRoute');
@@ -30,5 +31,15 @@ export const HomeRoutes: RouteObject[] = [
   {
     path: '/property/:id',
     element: <SinglePropertyRoute />,
+  },
+  {
+    path: '/host/homes',
+    element: <AuthLayout protectedRoute={true} />,
+    children: [
+      {
+        path: '',
+        element: <CreateAccommodationRoute />,
+      },
+    ],
   },
 ];
