7feb42f3e61b95b5a9d4779943eafef93a6d9081
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-01 11:04:23 +0500

    feat: acco form data interface created

---
+++ src/modules/home/types/accommodation-form-data.interface.ts
@@ -0,0 +1,8 @@
+import { type Accommodation } from './accommodation.type';
+
+export interface AccommodationFormData
+  extends Omit<Accommodation, 'id' | 'createdAt' | 'updatedAt' | 'images' | 'isAddedToWishlist'> {
+  categoryId: string;
+  amenities: string[];
+  images?: (string | File)[];
+}
