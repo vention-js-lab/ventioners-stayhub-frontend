a98d6cac2b872a33db004db0df0b7bfa4099a22a
purple-mountain <lmwrong92@gmail.com>
2024-12-05 22:59:57 +0000

    fix: updated skeleton sizes to exactly match real property card

--- src/modules/home/skeletons/property-card.skeleton.tsx
+++ src/modules/home/skeletons/property-card.skeleton.tsx
@@ -5,11 +5,11 @@ import { propertyCardSkeletonStyles } from './property-card.skeleton.styles';
 export function PropertyCardSkeleton() {
   return (
     <Box sx={propertyCardSkeletonStyles.container}>
-      <Skeleton variant="rectangular" height={200} width="100%" />
+      <Skeleton variant="rectangular" height={240} width="100%" />
       <Box sx={propertyCardSkeletonStyles.content}>
         <Skeleton width="60%" height={28} />
-        <Skeleton width="40%" height={20} sx={{ mt: 1 }} />
-        <Skeleton width="80px" height={20} sx={{ mt: 2 }} />
+        <Skeleton width="40%" height={20} sx={{ mt: 0.5 }} />
+        <Skeleton width="80px" height={20} sx={{ mt: 0.5 }} />
       </Box>
     </Box>
   );
