8c34f6c2d5b6d50547ae86ce90fa3dca450b8bfe
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-09 12:16:01 +0500

    fix: Stripe navigation method changed

--- src/modules/home/api/create-payment-stripe-checkout.ts
+++ src/modules/home/api/create-payment-stripe-checkout.ts
@@ -4,17 +4,15 @@ import { toast } from 'react-toastify';
 import { api } from '#/configs';
 import { type AxiosError } from 'axios';
 import { type CreateStripeCheckout, type CreateStripeCheckoutAxiosResponse } from '#/modules/home/types/payments';
-import { useNavigate } from 'react-router-dom';
 
 export const useCreatePaymentStripeCheckout = () => {
-  const navigate = useNavigate();
   return useMutation({
     mutationFn: async (data: CreateStripeCheckout) => {
       const response = await api.post<CreateStripeCheckoutAxiosResponse>(`${ENDPOINTS.payments}/stripe-checkout`, data);
       return response.data.data;
     },
     onSuccess: (checkoutSessionUrl: string) => {
-      navigate(`${checkoutSessionUrl}`);
+      window.location.href = checkoutSessionUrl;
     },
     onError: (error: AxiosError) => {
       toast.error(`Error creating booking: ${error.message}`);
