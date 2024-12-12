ca336b35f63ecc0963f53ca95ceb53d498c6cde0
Akilbek Urinbaev <akilbekurinbaev0616@gmail.com>
2024-12-12 19:20:22 +0500

    fix: small error

--- src/modules/home/api/get-booking-record.ts
+++ src/modules/home/api/get-booking-record.ts
@@ -1,28 +1,10 @@
 import { api } from '#/configs/axios.config';
-import { type User } from '#/types';
 import { useQuery } from '@tanstack/react-query';
 import { ENDPOINTS } from '../constants/endpoints.constant';
-import { type Accommodation } from '../types/accommodation.type';
+import { type Booking } from '../types/bookings/booking.type';
 
-export type Booking = {
-  id: string;
-  user: User;
-  accommodation: Accommodation;
-  checkInDate: string;
-  checkOutDate: string;
-  totalPrice: number;
-  numberOfGuests: number;
-  status: string;
-  createdAt: string;
-  updatedAt: string;
-};
-
-type GetUserBookingsResponse = {
-  data: Booking[];
-};
-
-export async function getUserBookings(): Promise<GetUserBookingsResponse> {
-  const response = await api.get<GetUserBookingsResponse>(ENDPOINTS.bookings);
+export async function getUserBookings(): Promise<Booking[]> {
+  const response = await api.get<Booking[]>(ENDPOINTS.bookings);
   return response.data;
 }
 
