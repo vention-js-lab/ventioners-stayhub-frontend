ef0a5e17cd9c87569007df7e552c8d8a3a2abb41
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-12 12:15:53 +0500

    fix: BookingsRoutes -> private routes

--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -1,6 +1,5 @@
 import { AuthRoutes } from '#/modules/auth';
-import { BookingsRoutes } from '#/modules/bookings';
 import { HomeRoutes } from '#/modules/home';
 import { composeModuleRoutes } from '#/utils/router.util';
 
-export const publicRoutes = composeModuleRoutes(HomeRoutes, AuthRoutes, BookingsRoutes);
+export const publicRoutes = composeModuleRoutes(HomeRoutes, AuthRoutes);
