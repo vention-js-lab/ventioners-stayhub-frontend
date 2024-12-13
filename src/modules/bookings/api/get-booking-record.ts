be1358db2bc15caa0221eea4891d90200c9ec719
Akilbek Urinbaev <akilbekurinbaev0616@gmail.com>
2024-12-13 12:08:20 +0500

    updates branch

--- src/modules/bookings/api/get-booking-record.ts
+++ src/modules/bookings/api/get-booking-record.ts
@@ -1,7 +1,7 @@
 import { api } from '#/configs/axios.config';
 import { useQuery } from '@tanstack/react-query';
 import { ENDPOINTS } from '../constants/endpoints.constant';
-import { type Booking } from '../types/bookings/booking.type';
+import { type Booking } from '../types';
 
 export async function getUserBookings(): Promise<Booking[]> {
   const response = await api.get<Booking[]>(ENDPOINTS.bookings);
