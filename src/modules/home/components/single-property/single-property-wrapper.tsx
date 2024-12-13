b58ac107113b3716ed387d944331012dba81e3b7
purple-mountain <lmwrong92@gmail.com>
2024-12-13 13:49:32 +0000

    fix: fix image order on details page

--- src/modules/home/components/single-property/single-property-wrapper.tsx
+++ src/modules/home/components/single-property/single-property-wrapper.tsx
@@ -7,9 +7,10 @@ import 'swiper/css';
 import { useState } from 'react';
 import { SinglePropertyImages } from './single-property-images.route';
 import Dialog from '@mui/material/Dialog';
+import { type Image } from '../../types/image.type';
 
 interface LargeViewProps {
-  images: string[];
+  images: Image[];
   onImageClick: () => void;
 }
 
@@ -17,21 +18,21 @@ function LargeView({ images, onImageClick }: LargeViewProps) {
   return (
     <Box sx={singlePropertyStyles.mainBox}>
       <Box sx={{ width: { xs: '100%', md: '50%' } }}>
-        <CardMedia component="img" image={images[0]} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
+        <CardMedia component="img" image={images[0].url} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
       </Box>
 
       <Box sx={singlePropertyStyles.imageBox}>
         <Box>
-          <CardMedia component="img" image={images[1]} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
+          <CardMedia component="img" image={images[1].url} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
         </Box>
         <Box>
-          <CardMedia component="img" image={images[2]} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
+          <CardMedia component="img" image={images[2].url} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
         </Box>
         <Box>
-          <CardMedia component="img" image={images[3]} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
+          <CardMedia component="img" image={images[3].url} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
         </Box>
         <Box>
-          <CardMedia component="img" image={images[4]} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
+          <CardMedia component="img" image={images[4].url} alt="Property Image" sx={{ objectFit: 'cover', height: '100%' }} />
         </Box>
       </Box>
       <Box onClick={onImageClick} style={singlePropertyStyles.viewMoreLink}>
@@ -46,10 +47,10 @@ function MobileView({ images, onImageClick }: LargeViewProps) {
     <Box>
       <Swiper loop={true} pagination={{ clickable: true }}>
         {images.map((image) => (
-          <SwiperSlide key={image}>
+          <SwiperSlide key={image.id}>
             <CardMedia
               component="img"
-              image={image}
+              image={image.url}
               alt="Property Image"
               sx={{ objectFit: 'cover', width: '100%', aspectRatio: '3/2', cursor: 'pointer' }}
               onClick={onImageClick}
@@ -61,7 +62,7 @@ function MobileView({ images, onImageClick }: LargeViewProps) {
   );
 }
 
-function PropertyImagesWrapper({ images }: { images: string[] }) {
+function PropertyImagesWrapper({ images }: { images: Image[] }) {
   const isMobile = useMediaQuery('(max-width:700px)');
   const [openModal, setOpenModal] = useState(false);
 
