71af388c522073160fc4be48780686901b964617
purple-mountain <lmwrong92@gmail.com>
2024-12-17 16:13:30 +0000

    fix: made details page public and fixed toast message

--- src/routes/private.route.ts
+++ src/routes/private.route.ts
@@ -2,14 +2,12 @@ import { BookingsRoutes } from '#/modules/bookings';
 import { ProfileRoutes } from '#/modules/users';
 import { CreateAccommodationRoutes, OwnerPropertiesRoutes } from '#/modules/owner';
 import { composeModuleRoutes } from '#/utils/router.util';
-import { SinglePropertyRoutes } from '#/modules/property-detail';
 import { WishlistRoutes } from '#/modules/wishlist';
 
 export const privateRoutes = composeModuleRoutes(
   ProfileRoutes,
   BookingsRoutes,
   OwnerPropertiesRoutes,
-  SinglePropertyRoutes,
   WishlistRoutes,
   CreateAccommodationRoutes
 );
--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -1,5 +1,6 @@
 import { AuthRoutes } from '#/modules/auth';
 import { HomeRoutes } from '#/modules/home';
+import { SinglePropertyRoutes } from '#/modules/property-detail';
 import { composeModuleRoutes } from '#/utils/router.util';
 
-export const publicRoutes = composeModuleRoutes(HomeRoutes, AuthRoutes);
+export const publicRoutes = composeModuleRoutes(HomeRoutes, AuthRoutes, SinglePropertyRoutes);
