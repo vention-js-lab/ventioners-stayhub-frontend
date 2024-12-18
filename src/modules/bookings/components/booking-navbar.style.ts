513d0eb3ee38fc4a005372dcf290352e098e3ee1
Akilbek Urinbaev <akilbekurinbaev0616@gmail.com>
2024-12-19 00:09:08 +0500

    added media style for booking filter

--- src/modules/bookings/components/booking-navbar.style.ts
+++ src/modules/bookings/components/booking-navbar.style.ts
@@ -3,12 +3,21 @@ import { type SxProps, type Theme } from '@mui/material/styles';
 export const navStyles = {
   container: {
     display: 'flex',
+    flexDirection: 'row',
     marginBottom: '18px',
   },
   header: {
     mb: 5,
     fontWeight: 300,
     textAlign: 'center',
+  },
+  menuStyles: {
+    width: '80%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0 auto',
+    backgroundColor: 'black',
   },
   navItem: (isSelected: boolean): SxProps<Theme> => ({
     width: '100%',
