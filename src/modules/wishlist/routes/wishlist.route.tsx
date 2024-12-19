b25f6ce63157e70a225eb723dbfee72ffe81b6ff
tiescl <anvar.madvaliev@icloud.com>
2024-12-20 01:26:54 +0500

    feat: add a spinner to wishlist page

--- src/modules/wishlist/routes/wishlist.route.tsx
+++ src/modules/wishlist/routes/wishlist.route.tsx
@@ -1,16 +1,26 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
 import { wishlistRouteStyles } from '../../wishlist/routes/wishlist.route.styles';
 import { HeaderComponent } from '#/modules/home/components/header';
 import { PropertyList } from '#/modules/home/components/property/property-list';
 import { useWishlistedProperties } from '../api/get-wishlisted-properties';
 import { useTranslation } from 'react-i18next';
 import { TRANSLATION_KEYS } from '#/constants/translation-keys.constant';
+import { loadingSpinnerStyles as styles } from '#/styles';
 
 export function WishlistRoute() {
   const { t } = useTranslation('wishlist');
   const { isLoading, data } = useWishlistedProperties();
+
+  if (isLoading) {
+    return (
+      <Box sx={styles.container}>
+        <CircularProgress sx={styles.spinner} />
+      </Box>
+    );
+  }
 
   return (
     <Box sx={wishlistRouteStyles.container}>
