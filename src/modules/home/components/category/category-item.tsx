2cd4bd6166a0737d1ecd8d56bea3749f76357aa4
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-12-19 14:16:47 +0500

    sceleton for category

--- src/modules/home/components/category/category-item.tsx
+++ src/modules/home/components/category/category-item.tsx
@@ -4,6 +4,7 @@ import { categoryStyles } from './category-item.styles';
 import { IconWrapper } from '../icon/icon-wrapper';
 import { type IconComponent } from '../../types/icon.type';
 import { useLanguage } from '#/contexts/language.context';
+import Skeleton from '@mui/material/Skeleton';
 
 type CategoryItemProps = {
   icon: IconComponent;
@@ -29,6 +30,23 @@ export function CategoryItem({ icon: Icon, name, name_ru, isActive, onClick }: C
       </Box>
       <Typography component="div" align="center" fontSize={13}>
         {language === 'en' ? name : name_ru}
+      </Typography>
+    </Box>
+  );
+}
+
+export function CategoryItemSkeleton() {
+  return (
+    <Box
+      sx={{
+        ...categoryStyles.categoryItemContainer,
+      }}
+    >
+      <Box sx={categoryStyles.iconBox}>
+        <Skeleton variant="circular" width={40} height={40} />
+      </Box>
+      <Typography component="div" align="center" fontSize={13}>
+        <Skeleton width={60} />
       </Typography>
     </Box>
   );
